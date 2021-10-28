d04a24218846b0a5af08cea0b1c229b9707ec6cf
Berkan Marasli <berkan.marasli@hotmail.com>
2021-10-28 17:58:45 +0100

    Found bug in Login - semi fixed

--- more-than-metrics-app/src/Components/Login/Login.js
+++ more-than-metrics-app/src/Components/Login/Login.js
@@ -1,9 +1,9 @@
-import React from "react";
-import LoginForm from "./LoginForm";
-import Dropdown from "../../Menu/Dropdown";
-import { makeStyles } from "@material-ui/styles";
+import React from "react"
+import LoginForm from "./LoginForm"
+import Dropdown from "../../Menu/Dropdown"
+import { makeStyles } from "@material-ui/styles"
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     fontFamily: "Lato",
     display: "flex",
@@ -11,39 +11,40 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     height: "100vh",
   },
-}));
+}))
 
 function Login() {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <div className={classes.root}>
       <Dropdown />
       <LoginForm getUser={getUser} />
     </div>
-  );
+  )
 }
 
 async function getUser(values, setError) {
-  console.log(`Welcome ${values.email} your password is ${values.password}`);
+  console.log(`Welcome ${values.email} your password is ${values.password}`)
 
-  const url = "http://localhost:8080/login";
+  const url = "http://localhost:8080/login"
 
   try {
     const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
-    });
-    const json = await response.json();
+    })
+    const json = await response.json()
 
-    if (!json.msg) {
-      return "Invalid username/password";
+    if (response.status !== 200) {
+      console.log(json.message)
     } else {
-      return "";
+      console.log("Logged in")
+      return
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-export default Login;
+export default Login
