47c94cc0361fc3df8173dd1810e63d00058e138c
kashcoding <88490912+kashcoding@users.noreply.github.com>
2021-11-01 17:35:20 +0000

    Application

--- more-than-metrics-app/src/Menu/About.js
+++ more-than-metrics-app/src/Menu/About.js
@@ -1,6 +1,6 @@
 import Dropdown from "./Dropdown"
 import { makeStyles } from "@material-ui/core/styles"
-import { Card, CardContent, Typography, CardActions, CardMedia, Button } from "@mui/material"
+import { Card, CardContent, Typography, CardActions, CardMedia, Button, Chip } from "@mui/material"
 import { LinkedIn, GitHub } from "@material-ui/icons"
 
 const useStyles = makeStyles((theme) => ({
@@ -11,6 +11,17 @@ const useStyles = makeStyles((theme) => ({
         alignItems: "center",
         height: "100vh",
     },
+
+    cards: {
+        display: "flex",
+        flexDirection: "row",
+    },
+
+    card: {
+        margin: "0.5rem",
+        height: "30rem",
+        width: "20rem",
+    },
 }))
 
 function About() {
@@ -20,66 +31,73 @@ function About() {
         <div className={classes.root}>
             <Dropdown />
             <h1 style={{ paddingTop: "5rem" }}>About us</h1>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia component="img" alt="" height="140" image="/assets/MTM/Berkan.jpg" />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <LinkedIn />
-                    <GitHub />
-                </CardActions>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia component="img" alt="" height="140" image="/assets/MTM/Kasia.jpg" />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <LinkedIn />
-                    <GitHub />
-                </CardActions>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia component="img" alt="" height="140" image="/assets/MTM/Kobi.jpg" />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <LinkedIn />
-                    <GitHub />
-                </CardActions>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia component="img" alt="" height="140" image="/assets/MTM/Sang.jpg" />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <LinkedIn />
-                    <GitHub />
-                </CardActions>
-            </Card>
+            <div className={classes.cards}>
+                <Card className={classes.card} sx={{ maxWidth: 345 }}>
+                    <CardMedia component="img" alt="" height="60%" image="/assets/MTM/Berkan.jpg" />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Berky boi <Chip label="Chip Filled" style={{ backgroundColor: "#FFBF50" }} />
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except
+                            Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <a href="https://www.linkedin.com/in/berkan-m-3777a9ba/" style={{ color: "inherit" }}>
+                            <LinkedIn style={{ color: "#FFBF50" }} />
+                        </a>
+                        <GitHub style={{ color: "#FFBF50" }} />
+                    </CardActions>
+                </Card>
+                <Card className={classes.card} sx={{ maxWidth: 345 }}>
+                    <CardMedia component="img" alt="" height="60%" image="/assets/MTM/Kasia.jpg" />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Kasia <Chip label="Chip Outlined" variant="outlined" />
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except
+                            Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <LinkedIn style={{ color: "#FFBF50" }} />
+                        <GitHub style={{ color: "#FFBF50" }} />
+                    </CardActions>
+                </Card>
+                <Card className={classes.card} sx={{ maxWidth: 345 }}>
+                    <CardMedia component="img" alt="" height="60%" image="/assets/MTM/Kobi.jpg" />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Kobi <Chip label="Chip Filled" style={{ backgroundColor: "#FFBF50" }} />
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except
+                            Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <LinkedIn style={{ color: "#FFBF50" }} />
+                        <GitHub style={{ color: "#FFBF50" }} />
+                    </CardActions>
+                </Card>
+                <Card className={classes.card} sx={{ maxWidth: 345 }}>
+                    <CardMedia component="img" alt="" height="60%" image="/assets/MTM/Sang.jpg" />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Sang <Chip label="Chip Filled" style={{ backgroundColor: "#FFBF50" }} />
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except
+                            Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <GitHub />
+                    </CardActions>
+                </Card>
+            </div>
         </div>
     )
 }
