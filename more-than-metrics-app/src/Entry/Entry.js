8a7b2e1f7c1a3435ef8fc4690c4039d49173ac3f
kashcoding <88490912+kashcoding@users.noreply.github.com>
2021-11-03 11:45:45 +0000

    Pull

--- more-than-metrics-app/src/Entry/Entry.js
+++ more-than-metrics-app/src/Entry/Entry.js
@@ -1,48 +1,33 @@
-import { CssBaseline } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import Welcome from "./Welcome";
-import ChooseUser from "./ChooseUser";
+import { CssBaseline } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import Welcome from "./Welcome"
+import ChooseUser from "./ChooseUser"
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: "100vh",
-    backgroundImage: `url(${
-      process.env.PUBLIC_URL + "/assets/recruitment2.jpeg"
-    })`,
-    backgroundPosition: "top",
-    backgroundSize: "85%",
-    backgroundRepeat: "no-repeat",
-    backgroundColor: "#FFFFFF",
-    fontFamily: "Lato",
-    [theme.breakpoints.down("md")]: {
-      backgroundSize: "150%",
+    root: {
+        minHeight: "100vh",
+        backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/recruitment2.jpeg"})`,
+        backgroundPosition: "top",
+        backgroundSize: "85%",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "#FFFFFF",
+        fontFamily: "Lato",
+        [theme.breakpoints.down("md")]: {
+            backgroundSize: "150%",
+        },
     },
-  },
-}));
+}))
 
 function Entry(props) {
-  const classes = useStyles();
+    const classes = useStyles()
 
-  return (
-    <div
-      className={classes.root}
-      style={
-        props.userType === null
-          ? {
-              backgroundImage: `url(${
-                process.env.PUBLIC_URL + "/assets/recruitment2.jpeg"
-              })`,
-              backgroundSize: "95%",
-              backgroundRepeat: "no-repeat",
-            }
-          : null
-      }
-    >
-      <CssBaseline />
-      <Welcome userType={props.userType} />
-      <ChooseUser setUserType={props.setUserType} />
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Welcome userType={props.userType} />
+            <ChooseUser setUserType={props.setUserType} />
+        </div>
+    )
 }
 
-export default Entry;
+export default Entry
