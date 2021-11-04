b97afb33fd380dfcf5dccbe575885ddf5f705517
Berkan Marasli <berkan.marasli@hotmail.com>
2021-11-04 14:48:01 +0000

    Fixed alignment of company name and logo on JobBoard

--- more-than-metrics-app/src/Components/ViewCompanyBtn.js
+++ more-than-metrics-app/src/Components/ViewCompanyBtn.js
@@ -2,7 +2,7 @@ import { Button } from "@material-ui/core"
 
 export default function ViewCompanyBtn(props) {
   return (
-    <Button style={{ border: "2px solid #FFBF50", borderRadius: "8px" }} onClick={props.handleOpen}>
+    <Button style={{ border: "2px solid #FFBF50", borderRadius: "8px", width: "150px" }} onClick={props.handleOpen}>
       {props.companyName}
     </Button>
   )
